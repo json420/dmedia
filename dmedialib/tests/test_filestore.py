0fa3b630b83bc25b83b33517437919d6c421b010
Jason Gerard DeRose <jderose@jasonderose.org>
2010-11-19 03:53:27 -0700

    Switch from LGPLv3 to AGPLv3; changed author/copyright to my @novacut.com email

--- dmedialib/tests/test_filestore.py
+++ dmedialib/tests/test_filestore.py
@@ -1,25 +1,24 @@
 # Authors:
-#   Jason Gerard DeRose <jderose@jasonderose.org>
-#   Akshat Jain <ssj6akshat1234@gmail.com>
+#   Jason Gerard DeRose <jderose@novacut.com>
+#   Akshat Jain <ssj6akshat1234@gmail.com)
 #
 # dmedia: distributed media library
-# Copyright (C) 2010 Jason Gerard DeRose <jderose@jasonderose.org>
+# Copyright (C) 2010 Jason Gerard DeRose <jderose@novacut.com>
 #
 # This file is part of `dmedia`.
 #
 # `dmedia` is free software: you can redistribute it and/or modify it under the
-# terms of the GNU Lesser General Public License as published by the Free
+# terms of the GNU Affero General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option) any
 # later version.
 #
 # `dmedia` is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
 # details.
 #
-# You should have received a copy of the GNU Lesser General Public License along
+# You should have received a copy of the GNU Affero General Public License along
 # with `dmedia`.  If not, see <http://www.gnu.org/licenses/>.
-
 
 """
 Unit tests for `dmedialib.filestore` module.
