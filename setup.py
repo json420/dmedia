507c1365ad6ec4856fe8fd10b444e506410df6e8
David Green <david4dev@gmail.com>
2010-11-28 14:42:41 +0000

    added indicator to scripts in setup.py

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ setup(
     package_data=dict(
         dmedialib=['data/*'],
     ),
-    scripts=['dmedia', 'dmedia-import'],
+    scripts=['dmedia', 'dmedia-import', 'indicator-dmedia'],
     data_files=[
         ('share/man/man1', ['dmedia.1']),
         ('share/applications', ['dmedia-import.desktop']),
