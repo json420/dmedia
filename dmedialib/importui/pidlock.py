f148abe1a50250af7a3152e5579eae1c89619035
David Green <david4dev@gmail.com>
2010-12-07 17:31:22 +0000

    Created tests for pidlock

--- dmedialib/importui/pidlock.py
+++ dmedialib/importui/pidlock.py
@@ -20,6 +20,8 @@
 #
 # You should have received a copy of the GNU Affero General Public License along
 # with `dmedia`.  If not, see <http://www.gnu.org/licenses/>.
+import os
+
 
 class PidLock(object):
     """
