7e6a9e9c3cc9c911ea1cc90f576673ef907bbeeb
Jason Gerard DeRose <jderose@novacut.com>
2011-01-26 09:23:49 -0700

    Bumped version to 0.4.0, another cycle begins

--- dmedia/__init__.py
+++ dmedia/__init__.py
@@ -31,7 +31,7 @@ so keep your hardhats on!  A good place to start is the `FileStore` class in the
 current code.
 """
 
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 
 import os
 from os import path
