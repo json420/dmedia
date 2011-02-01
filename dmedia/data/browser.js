14e6d6127b6d042dde6da9bf685a4a297a7d92db
Jason Gerard DeRose <jderose@novacut.com>
2011-02-01 15:05:39 -0700

    Sort of got get_data() working

--- dmedia/data/browser.js
+++ dmedia/data/browser.js
@@ -36,63 +36,57 @@ function load(rows) {
 };
 
 function get_data(doc){
-    console.log('get_data', doc);
-    return;
-	//var info = document.getElementById('info');
+    //console.log('get_data', doc);
+
+//	if (selected != "none"){
+//		//if something is selected, save the changes to it's values before loading the next object
+//		form = document.forms[0];
+//		title = form.elements["title"];
+//		tags = form.elements["tags"];
+//		description = form.elements["description"];
+//		notes = form.elements["notes"];
+//		set_data(selected, "title", title.value);
+//		set_data(selected, "tags", tags.value);
+//		set_data(selected, "description", description.value);
+//		set_data(selected, "notes", notes.value);
+//	};
+//	selected = doc._id;
+
 	var preview = document.getElementById('display');
-	if (selected != "none"){
-		//if something is selected, save the changes to it's values before loading the next object
-		form = document.forms[0];
-		title = form.elements["title"];
-		tags = form.elements["tags"];
-		description = form.elements["description"];
-		notes = form.elements["notes"];
-		set_data(selected, "title", title.value);
-		set_data(selected, "tags", tags.value);
-		set_data(selected, "description", description.value);
-		set_data(selected, "notes", notes.value);
+	var content = "<h2>Video Info</h2><p>";
+	content += "<b>File Name: </b>" + doc.basename + "<br>";
+	content += "<b>FPS: </b>" + doc.fps + "<br>";
+	content += "<b>Aperture: </b>" + doc.aperture + "<br>";
+	content += "<b>Focal Length: </b>" + doc.focal_length + "<br>";
+	content += "<b>Shutter: </b>" + doc.shutter + "<br>";
+	content += "<b>Camera: </b>" + doc.camera + "<br>";
+	content += "<b>Video Codec: </b>" + doc.codec_video + "<br>";
+	content += "<b>Resolution: </b>" + doc.width + "x" + doc.height + "<br>";
+	//info.innerHTML = content;
 
+	content += "<img src=\"data:";
+	content += doc._attachments.thumbnail.content_type;
+	content += ";base64,";
+	content += doc._attachments.thumbnail.data;
+	content += "\" width=\"192\" height=\"108\" align=\"center\"><br>";
 
-	};
-	selected = id;
+	content += "<form>";
+	content += "<b>Title: </b><input type=\"text\" class=\"field\" name=\"title\" value=\"" + doc.title + "\"><br>";
+	content += "<div class=\"star " + doc.rating + "\">" + doc.rating + "</div>";
+	content += "<b>Tags: </b><br><textarea class=\"field\" name=\"tags\" rows=\"5\" cols=\"34\">" + doc.tags + "</textarea><br>";
+	content += "<b>Description: </b><br><textarea class=\"field\" name=\"description\" rows=\"5\" cols=\"34\">" + doc.description + "</textarea><br>";
+	content += "<b>Notes: </b><br><textarea class=\"field\" name=\"notes\" rows=\"5\" cols=\"34\">" + doc.notes + "</textarea><br>";
+	content += "</form>";
+	preview.innerHTML = content;
 
-	for (item in data){
-		if (data[item]._id == id){
-			content = "<h2>Video Info</h2><p>";
-			content += "<b>File Name: </b>" + data[item].name + "<br>";
-			content += "<b>FPS: </b>" + data[item].fps + "<br>";
-			content += "<b>Aperture: </b>" + data[item].aperture + "<br>";
-			content += "<b>Focal Length: </b>" + data[item].focal_length + "<br>";
-			content += "<b>Shutter: </b>" + data[item].shutter + "<br>";
-			content += "<b>Camera: </b>" + data[item].camera + "<br>";
-			content += "<b>Video Codec: </b>" + data[item].codec_video + "<br>";
-			content += "<b>Resolution: </b>" + data[item].width + "x" + data[item].height + "<br>";
-			//info.innerHTML = content;
-
-			content += "<img src=\"data:";
-			content += data[item]._attachments.thumbnail.content_type;
-			content += ";base64,";
-			content += data[item]._attachments.thumbnail.data;
-			content += "\" width=\"192\" height=\"108\" align=\"center\"><br>";
-
-			content += "<form>";
-			content += "<b>Title: </b><input type=\"text\" class=\"field\" name=\"title\" value=\"" + data[item].title + "\"><br>";
-			content += "<div class=\"star " + data[item].rating + "\">" + data[item].rating + "</div>";
-			content += "<b>Tags: </b><br><textarea class=\"field\" name=\"tags\" rows=\"5\" cols=\"34\">" + data[item].tags + "</textarea><br>";
-			content += "<b>Description: </b><br><textarea class=\"field\" name=\"description\" rows=\"5\" cols=\"34\">" + data[item].description + "</textarea><br>";
-			content += "<b>Notes: </b><br><textarea class=\"field\" name=\"notes\" rows=\"5\" cols=\"34\">" + data[item].notes + "</textarea><br>";
-			content += "</form>";
-			preview.innerHTML = content;
-
-			oText = oForm.elements["tags"];
-			document.write(oText.value);
-		};
-	};
+	//oText = oForm.elements["tags"];
+	//document.write(oText.value);
 
 
 };
 
 function set_data(id, tag, value){
+    return;
 	for (item in dmedia.data){
 		if (dmedia.data[item]._id == id){
 			eval("data[item]." + tag + " = \"" + value + "\"")
