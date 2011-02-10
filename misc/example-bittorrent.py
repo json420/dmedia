57c3b8ede76eff265216aa74519dfce1243fd52a
Jason Gerard DeRose <jderose@novacut.com>
2011-02-10 16:45:24 -0700

    More cleanup on example-bittorrent.py

--- misc/example-bittorrent.py
+++ misc/example-bittorrent.py
@@ -1,3 +1,9 @@
+"""
+Tests downloading torrent from:
+
+http://37GDNHANX7RCBMBGTYLSIK7TMTUQSKDS
+"""
+
 from __future__ import print_function
 
 import sys
@@ -32,34 +38,43 @@ base = path.join(home, '.dmedia_test')
 store = FileStore(base)
 
 # Get tmp path we will write file to as we download:
-tmp = store.temp(chash, 'tgz', create=True)
+tmp = store.temp(chash, 'mov', create=True)
 print('Will write file to:\n  %r\n' % tmp)
 
 # Path of torret file in misc/
-torrent = path.join(
+dot_torrent = path.join(
     path.dirname(path.abspath(__file__)),
     'example.torrent'
 )
-assert path.isfile(torrent), torrent
-print('Using torrent:\n  %r\n' % torrent)
+assert path.isfile(dot_torrent), dot_torrent
+print('Using torrent:\n  %r\n' % dot_torrent)
 
 
 session = libtorrent.session()
 session.listen_on(6881, 6891)
 
-e = libtorrent.bdecode(open(torrent, 'rb').read())
+e = libtorrent.bdecode(open(dot_torrent, 'rb').read())
 info = libtorrent.torrent_info(e)
 
-h = session.add_torrent({
+torrent = session.add_torrent({
     'ti': info,
     'save_path': path.dirname(tmp),
 })
 
 
-while not h.is_seed():
-    s = h.status()
+while not torrent.is_seed():
+    s = torrent.status()
     print(s.progress)
     time.sleep(2)
 
 
-print(h.name())
+session.remove_torrent(torrent)
+time.sleep(1)
+
+tmp_fp = open(tmp, 'rb')
+os.fchmod(tmp_fp.fileno(), 0o444)
+h = HashList(tmp_fp)
+got = h.run()
+if got != chash:
+    print('download failed: got %r; expected %r' % (got, chash))
+os.rename(tmp, store.path(chash, 'mov', create=True))
