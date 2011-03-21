349061269a80d12c7cbe162a6a2aa51d2b86d806
Jason Gerard DeRose <jderose@novacut.com>
2011-03-21 13:01:34 -0600

    Stubbed out couch.CouchBase API

--- dmedia/data/couch.js
+++ dmedia/data/couch.js
@@ -1,3 +1,19 @@
+/*
+JavaScript port of Python3 `microfiber` CouchDB adapter:
+
+    https://launchpad.net/microfiber
+
+    http://bazaar.launchpad.net/~microfiber/microfiber/trunk/view/head:/microfiber.py
+
+Rather than inventing an API, this is a simple adapter for calling a REST JSON
+API like CouchDB.  The goal is to make something that doesn't need constant
+maintenances as additional features are added to the CouchDB API.
+
+For some good documentation of the CouchDB REST API, see:
+
+    http://docs.couchone.com/couchdb-api/
+*/
+
 var couch = {};
 
 couch.stuff = function() {
@@ -6,4 +22,38 @@ couch.stuff = function() {
 
 couch.awesome = function(person) {
     return person + ' is awesome!';
+}
+
+// microfiber.CouchBase
+couch.CouchBase = function(url) {
+    this.url = url;
+}
+couch.CouchBase.prototype = {
+    post: function(obj, parts, options) {
+
+    },
+
+    put: function(obj, parts, options) {
+
+    },
+
+    get: function(parts, options) {
+
+    },
+
+    delete: function(parts, options) {
+
+    },
+
+    head: function(parts, options) {
+
+    },
+
+    put_att: function(mime, data, parts, options) {
+
+    },
+
+    get_att: function(parts, options) {
+
+    },
 }
