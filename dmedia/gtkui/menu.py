bc710c57dfe834357201b36e9ea6dea66ddb92e2
David Green <david4dev@gmail.com>
2011-04-14 22:16:45 +0100

    forgot to bzr add menu.py! adding it now

---
+++ dmedia/gtkui/menu.py
@@ -0,0 +1,42 @@
+# Authors:
+#   David Green <david4dev@gmail.com>
+#
+# dmedia: distributed media library
+# Copyright (C) 2010 Jason Gerard DeRose <jderose@novacut.com>
+#
+# This file is part of `dmedia`.
+#
+# `dmedia` is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# `dmedia` is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with `dmedia`.  If not, see <http://www.gnu.org/licenses/>.
+
+from gi.repository import Gtk
+from gettext import gettext as _
+
+ACTIONS = {
+    "close" : Gtk.main_quit
+}
+
+
+MENU = [
+    {
+        "label" : _("File"),
+        "type" : "menu",
+        "items" : [
+            {
+                "label" : _("Close"),
+                "type" : "action",
+                "action" : "close"
+            }
+        ]
+    }
+]
