790dc8b833ba4323b0755fd749559d9eee6b779d
Jason Gerard DeRose <jderose@novacut.com>
2011-04-17 03:08:34 -0600

    primative, but working keep (enter) or reject (spacebar) workflow

--- dmedia/webui/data/inbox.js
+++ dmedia/webui/data/inbox.js
@@ -1,28 +1,101 @@
 "use strict";
 
-var dmedia = {
-    db: new couch.Database('dmedia', '/'),
+function Inbox(id, db) {
+    this.el = $(id);
+    this.db = db;
+
+    var self = this;
+    window.addEventListener('keypress',
+        function (event) {self.on_keypress(event)},
+        true
+    );
+
+    this.load();
+}
+Inbox.prototype = {
+    on_keypress: function(event) {
+        if (event.charCode == 32) {
+            // spacebar
+            event.preventDefault();
+            this.reject();
+        }
+        else if (event.keyCode == 13) {
+            // enter
+            event.preventDefault();
+            this.keep();
+        }
+    },
 
     load: function() {
-        var result = dmedia.db.view('user', 'inbox', {
+        var result = this.db.view('user', 'inbox', {
             include_docs: true,
             reduce: false,
-            limit: 10,
+            limit: 50,
         });
         var inbox = $el('div', {'id': 'inbox'});
         $appendEach(inbox, result, function(doc) {
-            var box = $el('div', {'class': 'item'});
-            $appendMeta(box, data.meta, doc.meta, function(label, value) {
-                var p = $el('p');
-                p.appendChild($el('strong', {textContent: label + ': '}));
-                p.appendChild($el('span', {textContent: value}));
-                return p;
+            var box = $el('div', {
+                'class': 'item',
+                'id': doc._id,
+                'textContent': doc.name,
             });
+            box._doc = doc;
             return box;
         });
-        $replace('inbox', inbox);
+        $replace(this.el, inbox);
+        this.el = inbox;
 
+        if (this.el.childNodes.length > 0) {
+            this.select(this.el.childNodes[0]);
+        }
+    },
+
+    select: function(el) {
+        if (this.selected) {
+            this.selected.classList.remove('selected');
+        }
+        if (! el) {
+            this.selected = null;
+            return;
+        }
+        this.selected = el;
+        this.selected.classList.add('selected');
+        this.selected.scrollIntoView();
+    },
+
+    next: function() {
+        if (this.selected) {
+            this.select(this.selected.nextSibling);
+        }
+    },
+
+    set_status: function(status) {
+        if (! this.selected) {
+            return;
+        }
+        this.selected.classList.add(status);
+        this.selected._doc.status = status;
+        this.db.save(this.selected._doc);
+        this.next();
+    },
+
+    keep: function() {
+        this.set_status('keep');
+    },
+
+    reject: function() {
+        this.set_status('reject');
     },
 }
 
-window.onload = dmedia.load
+
+var dmedia = {
+    db: new couch.Database('dmedia', '/'),
+
+    on_load: function() {
+        dmedia.inbox = new Inbox('inbox', dmedia.db);
+    },
+
+}
+
+window.addEventListener('load', dmedia.on_load, false);
