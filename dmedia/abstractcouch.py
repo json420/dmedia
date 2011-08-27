167965062cce1ac15c90da32fba3f511fa063092
Jason Gerard DeRose <jderose@novacut.com>
2011-08-27 15:57:17 -0600

    Revamped abstractcouch.py to use microfiber and /usr/bin/abstractcouch

--- dmedia/abstractcouch.py
+++ dmedia/abstractcouch.py
@@ -38,109 +38,32 @@ For more details, see:
 """
 
 import json
+from subprocess import check_output
 import logging
 
-from couchdb import Server, ResourceNotFound
-try:
-    from desktopcouch.records.http import OAuthSession
-except ImportError:
-    OAuthSession = None
+from microfiber import Database  # Hooray!
 
 
 log = logging.getLogger()
 
 
-def get_server(env):
-    """
-    Return `couchdb.Server` for desktopcouch or system-wide CouchDB.
-
-    The *env* argument is a ``dict`` instance containing information about how
-    to connect to CouchDB.  It must always contain a ``"url"`` key, which is the
-    URL as passed to ``couchdb.Server()``.
-
-    If *env* contains an ``"oauth"`` key, a
-    ``desktopcouch.records.http.OAuthSession`` will be created and passed to
-    ``couchdb.Server()``.
-
-    For example, to connect to the system-wide CouchDB, pass an *env* like this:
-
-    >>> env = {'url': 'http://localhost:5984/'}
-
-    Or to connect to a per-user desktopcouch CouchDB, pass an *env* like this:
-
-    >>> env = {
-    ...     'url': 'http://localhost:51074/',
-    ...     'oauth': {
-    ...         'consumer_secret': 'no',
-    ...         'token': 'way',
-    ...         'consumer_key': 'too',
-    ...         'token_secret': 'secret'
-    ...     }
-    ... }
-
-    When using desktopcouch, you can build an *env* with the correct port and
-    oauth credentials like this:
-
-    >>> from desktopcouch.application.platform import find_port
-    >>> from desktopcouch.application.local_files import get_oauth_tokens
-    >>> env = {
-    ...     'url': 'http://localhost:%d/' % find_port(),
-    ...     'oauth': get_oauth_tokens(),
-    ... }
-
-    Note the reason *env* is a ``dict`` is so it's easily extensible and can
-    carry other useful information, for example the dmedia database name so that
-    an alternate name can be provided for, say,  unit tests (like
-    ``"dmedia_test"``).
-
-    The goal is to have all the needed information is one easily serialized
-    piece of data (important for testing across multiple processes).
-    """
-    url = env['url']
-    log.info('CouchDB server is %r', url)
-    if 'oauth' in env:
-        if OAuthSession is None:
-            raise ValueError(
-                "provided env['oauth'] but OAuthSession not available: %r" %
-                    (env,)
-            )
-        log.info('Using desktopcouch `OAuthSession`')
-        session = OAuthSession(credentials=env['oauth'])
-    else:
-        session = None
-    return Server(url, session=session)
-
-
-def get_db(env, server=None):
+def get_db(env):
     """
     Return the CouchDB database specified by *env*.
 
     The database name is determined by ``env['dbname']``.
 
-    If the database does not exist, it will be created.
-
-    If *server* is ``None``, one is created based on *env* by calling
-    `get_server()`.
-
-    Returns a ``couchdb.Database`` instance.
+    Returns a ``microfiber.Database`` instance.
     """
-    if server is None:
-        server = get_server(env)
     dbname = env['dbname']
     log.info('CouchDB database is %r', dbname)
-    try:
-        return server[dbname]
-    except ResourceNotFound:
-        return server.create(dbname)
+    return Database(dbname, env['url'], env.get('oauth'), env.get('basic'))
 
 
-def load_env(env_s):
+def get_env(dbname='dmedia'):
+    env_s = check_output(['/usr/bin/abstractcouch'])
     env = json.loads(env_s)
-    # FIXME: hack to work-around for Python oauth not working with unicode,
-    # which is what we get when the env is retrieved over D-Bus as JSON
-    if 'oauth' in env:
-        env['oauth'] = dict(
-            (k.encode('ascii'), v.encode('ascii'))
-            for (k, v) in env['oauth'].iteritems()
-        )
+    # FIXME: oauth is broken on desktopcouch in Oneiric:
+    env['oauth'] = None
+    env['dbname'] = dbname
     return env
