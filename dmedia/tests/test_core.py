7a96fd40d15c6b76e1e8cf5ef79c79c72e85be45
Jason Gerard DeRose <jderose@novacut.com>
2011-10-10 08:38:02 -0600

    service3 now actually starts Core

--- dmedia/tests/test_core.py
+++ dmedia/tests/test_core.py
@@ -42,6 +42,40 @@ class TestCore(CouchCase):
         self.assertIsInstance(inst.db, microfiber.Database)
         self.assertEqual(inst.db.name, 'dmedia')
         self.assertIsInstance(inst.stores, LocalStores)
+        self.assertEqual(inst.local['stores'], {})
+
+        # Add some filestores
+        tmp1 = TempDir()
+        tmp2 = TempDir()
+        fs1 = inst.add_filestore(tmp1.dir, copies=2)
+        fs2 = inst.add_filestore(tmp2.dir)
+
+        # Test that these filestores are brought up next time
+        inst = core.Core(self.env)
+        self.assertEqual(inst.local['stores'],
+            {
+                tmp1.dir: {'id': fs1.id, 'copies': 2},
+                tmp2.dir: {'id': fs2.id, 'copies': 1},
+            }
+        )
+        self.assertEqual(inst.local, inst.db.get('_local/dmedia'))
+        
+        # Test that startup works even when a store is missing:
+        tmp1.rmtree()
+        inst = core.Core(self.env)
+        self.assertEqual(inst.local['stores'],
+            {
+                tmp2.dir: {'id': fs2.id, 'copies': 1},
+            }
+        )
+        self.assertEqual(inst.local, inst.db.get('_local/dmedia'))
+
+        # Test that startup works when store is missing and it brings us down to
+        # zero stores
+        tmp2.rmtree()
+        inst = core.Core(self.env)
+        self.assertEqual(inst.local['stores'], {})
+        self.assertEqual(inst.local, inst.db.get('_local/dmedia'))
 
     def test_init_local(self):
         inst = core.Core(self.env, bootstrap=False)
@@ -49,4 +83,49 @@ class TestCore(CouchCase):
         self.assertFalse(hasattr(inst, 'local'))
         self.assertIsNone(inst._init_local())
         self.assertEqual(inst.local, inst.db.get('_local/dmedia'))
+
+    def test_add_filestore(self):
+        inst = core.Core(self.env)
+        tmp = TempDir()
+
+        # Test adding new
+        fs = inst.add_filestore(tmp.dir)
+        self.assertIsInstance(fs, filestore.FileStore)
+        self.assertEqual(fs.parentdir, tmp.dir)
+        self.assertEqual(fs.copies, 1)
+        self.assertEqual(
+            inst.local['stores'],
+            {tmp.dir: {'id': fs.id, 'copies': 1}}
+        )
+        self.assertEqual(inst.stores.ids, {fs.id: fs})
+        self.assertEqual(inst.stores.parentdirs, {tmp.dir: fs})
+
+        # Test adding a duplicate
+        with self.assertRaises(Exception) as cm:
+            inst.add_filestore(tmp.dir)
+        self.assertEqual(
+            str(cm.exception),
+            'already have parentdir {!r}'.format(tmp.dir)
+        )
+        self.assertEqual(
+            inst.local['stores'],
+            {tmp.dir: {'id': fs.id, 'copies': 1}}
+        )
+        self.assertEqual(inst.stores.ids, {fs.id: fs})
+        self.assertEqual(inst.stores.parentdirs, {tmp.dir: fs})
+
+        # Test adding a duplicate on next startup
+        inst = core.Core(self.env)
+        with self.assertRaises(Exception) as cm:
+            inst.add_filestore(tmp.dir)
+        self.assertEqual(
+            str(cm.exception),
+            'already have parentdir {!r}'.format(tmp.dir)
+        )
+        self.assertEqual(
+            inst.local['stores'],
+            {tmp.dir: {'id': fs.id, 'copies': 1}}
+        )
+        self.assertEqual(set(inst.stores.ids), set([fs.id]))
+        self.assertEqual(set(inst.stores.parentdirs), set([tmp.dir]))
 
