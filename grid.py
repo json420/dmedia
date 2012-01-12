ababffbb3829e3130c9f4c9aaa9077e47bd35d90
Jason Gerard DeRose <jderose@novacut.com>
2012-01-12 00:24:47 -0700

    Added a quick grid generation script

---
+++ grid.py
@@ -0,0 +1,21 @@
+grid = """.grid_{} {{
+    width: {}px;
+}}
+"""
+
+
+def get_width(base, margin, cols):
+    gutter = margin * 2
+    return base * cols + gutter * (cols - 1)
+
+
+def iter_cols(grid_cols=16, base=40, margin=10):
+    for cols in range(1, grid_cols + 1):
+        yield (cols, get_width(base, margin, cols))
+
+
+# Or iter_cols(12, 60)
+
+for t in iter_cols():
+    print(grid.format(*t))
+
