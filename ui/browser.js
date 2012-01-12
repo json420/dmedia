5ff6b07246d894aca66e48c19d2989e31db76761
Jason Gerard DeRose <jderose@novacut.com>
2012-01-12 16:45:03 -0700

    Got <video> resizing reliably

--- ui/browser.js
+++ ui/browser.js
@@ -7,7 +7,32 @@ function css_url(url) {
     return ['url(', JSON.stringify(url), ')'].join('');
 }
 
+
+var UI = {
+    timeout_id: null,
+
+    init: function() {
+        UI.content = $('content');
+        UI.player = $el('video', {'id': 'player'});
+        UI.resize();
+        UI.content.appendChild(UI.player);
+        window.addEventListener('resize', UI.on_resize);
+    },
+
+    resize: function() {
+        UI.player.setAttribute('width', UI.content.clientWidth - 20);
+        UI.player.setAttribute('height', UI.content.clientHeight - 20);
+    },
+
+    on_resize: function() {
+        UI.content.removeChild(UI.player);
+        UI.resize();
+        UI.content.appendChild(UI.player);
+    },
+}
+
 window.onload = function() {
+    UI.init();
     var div = $('right');
     div.innerHTML = null;
     var rows = db.view_sync('user', 'video', {reduce: false, limit: 100})['rows'];
@@ -20,7 +45,8 @@ window.onload = function() {
             }
         );
         child.onclick = function() {
-            $select(id);
+            UI.player.src = 'dmedia:' + id;
+            UI.player.load();
         }
         var url = db.att_url(row.id, 'thumbnail');
 		child.style.backgroundImage = css_url(url);
