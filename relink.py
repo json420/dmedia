718343c1667cff675f78142e34309efdbf2eccbb
Jason Gerard DeRose <jderose@novacut.com>
2012-02-10 12:22:35 -0800

    Modernized relink.py

--- relink.py
+++ relink.py
@@ -3,29 +3,14 @@
 import sys
 from os import path
 
-from microfiber import dmedia_env, Conflict, NotFound
-from dmedia.util import get_db
+from microfiber import dmedia_env
+from dmedia.metastore import MetaStore
 from dmedia.core import init_filestore
 
 parentdir = path.abspath(sys.argv[1])
 
-db = get_db(dmedia_env())
+ms = MetaStore(dmedia_env())
 (fs, doc) = init_filestore(parentdir)
-try:
-    db.save(doc)
-except Conflict:
-    pass
-
-for st in fs:
-    print(st.id)
-    try:
-        doc = db.get(st.id)
-        doc['stored'][fs.id] = {
-            'copies': 1,
-            'mtime': st.mtime,
-            'plugin': 'filestore',
-        }
-        db.save(doc)
-    except NotFound:
-        pass
+ms.relink(fs)
+ms.scan(fs)
 
