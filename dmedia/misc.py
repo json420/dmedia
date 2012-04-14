3350e85d6b3decaace11c4d5b2b5d8948cb7f224
Jason Gerard DeRose <jderose@novacut.com>
2012-04-14 17:17:39 -0600

    Added dmedia.misc module with WeakMethod, added unit tests for WeakMethod

---
+++ dmedia/misc.py
@@ -0,0 +1,41 @@
+# dmedia: distributed media library
+# Copyright (C) 2012 Novacut Inc
+#
+# This file is part of `dmedia`.
+#
+# `dmedia` is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# `dmedia` is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with `dmedia`.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#   Jason Gerard DeRose <jderose@novacut.com>
+
+"""
+Don't know where else to put this stuff.
+"""
+
+import weakref
+
+
+class WeakMethod:
+    __slots__ = ('proxy', 'method_name')
+
+    def __init__(self, inst, method_name):
+        if not callable(getattr(inst, method_name)):
+            raise TypeError(
+                '{!r} attribute is not callable'.format(method_name)
+            )
+        self.proxy = weakref.proxy(inst)
+        self.method_name = method_name
+
+    def __call__(self, *args):
+        return getattr(self.proxy, self.method_name)(*args)
