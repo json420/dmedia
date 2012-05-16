6c51266afdcf10001a3064bd67095af11649f0d8
Jason Gerard DeRose <jderose@novacut.com>
2012-05-16 09:40:12 -0600

    Updated links.py to be more friendly

--- links.py
+++ links.py
@@ -1,22 +1,51 @@
-#!/usr/bin/python
-
-import sys
-from microfiber import Database, dmedia_env
-from filestore import FileStore
 import os
 from os import path
+import json
+import shutil
 
-parentdir = path.abspath(sys.argv[1])
+import dbus
+from filestore import FileStore
 
-db = Database('dmedia-0-ahgokhnaabgo4dhwtuh6d4p4', dmedia_env())
-fs = FileStore(parentdir)
+from dmedia.util import get_project_db
 
-rows = db.view('user', 'ctime')['rows']
-for (i, row) in enumerate(rows):
-    _id = row['id']
-    if not fs.exists(_id):
-        continue
-    target = fs.path(_id)
-    doc = db.get(_id)
-    link = path.join(fs.parentdir, 'links', '{}.{}'.format(i, doc['ext']))
-    os.symlink(target, link)
+
+session = dbus.SessionBus()
+Dmedia = session.get_object('org.freedesktop.Dmedia', '/')
+env = json.loads(Dmedia.GetEnv())
+fs = FileStore('/media/Novacut-1')
+
+projects = ('QSD2VXLVRW4AXNKO3HZJT5TC', 'E2M4VPIIYXIYTTXVGFITTRCV')
+#projects = ('E2M4VPIIYXIYTTXVGFITTRCV',)
+
+
+for project_id in projects:
+    db = get_project_db(project_id, env)
+    p = db.get(project_id)
+    print(project_id, p['title'])
+    base = path.join(fs.parentdir, p['title'])
+    if not path.isdir(base):
+        os.mkdir(base)
+    i = 0
+    for row in db.view('user', 'ctime')['rows']:
+        _id = row['id']
+        doc = db.get(_id)
+        ext = doc.get('ext')
+        if ext in ('mov', 'wav', 'cr2', 'jpg'):
+            i += 1
+            src = fs.path(_id)
+            d = path.join(base, ext)
+            if not path.isdir(d):
+                os.mkdir(d)
+            dst = path.join(d, '{:04d}.{}'.format(i, ext))
+            print(src)
+            print(dst)
+            os.symlink(src, dst)
+            #shutil.copy2(src, dst)
+
+
+#target = Dmedia.Resolve(_id)
+#target = fs.path(_id)
+#link = path.join('/media/NovacutSilver/links', )
+#print(link)
+#os.symlink(target, link) 
+
