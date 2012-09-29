5b2b65dd824c9f69ecc502e1d9d10f34c9b4fd16
Jason Gerard DeRose <jderose@novacut.com>
2012-09-28 22:59:02 -0600

    Spruced up benchmark-httpd.py

--- benchmark-httpd.py
+++ benchmark-httpd.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-from usercouch.misc import TempPKI
 import multiprocessing
-import json
-from microfiber import Server, dumps, build_ssl_context
+import optparse
 import time
-from dmedia.httpd import HTTPD, build_server_ssl_context, echo_app
+
+from usercouch.misc import TempPKI
+from microfiber import CouchBase, dumps
+from dmedia.httpd import run_server, echo_app
 
 
 def start_process(target, *args, **kwargs):
@@ -15,50 +16,47 @@ def start_process(target, *args, **kwargs):
     return process
 
 
-def server(queue, config):
-    ctx = build_server_ssl_context(config)
-    httpd = HTTPD(echo_app, '::1', ctx, True)
-    env = {'port': httpd.port, 'url': httpd.url}
-    queue.put(env)
-    httpd.serve_forever()
-
-
-def start_httpd(config):
-    q = multiprocessing.Queue()
-    httpd = start_process(server, q, config)
-    env = q.get()
-    return (httpd, env)
-
-
-def loop(env, count):
-    s = Server(env)
+def client_process(env, count):
+    client = CouchBase(env)
     for i in range(count):
-        s.get()
+        client.get()
 
 
-pki = TempPKI(client_pki=True)
-(httpd, env) = start_httpd(pki.get_server_config())
+parser = optparse.OptionParser()
+parser.add_option('--client-pki',
+    help='use client-side PKI also',
+    action='store_true',
+    default=False,
+)
+(options, args) = parser.parse_args()
+
+
+pki = TempPKI(options.client_pki)
+q = multiprocessing.Queue()
+start_process(run_server, q, echo_app,
+    ssl_config=pki.get_server_config(),
+    threaded=True,
+)
+env = q.get()
 env['ssl'] = pki.get_client_config()
-#print(dumps(env))
 
-s = Server(env)
-s.get()
-#print(dumps(s.get(), pretty=True))
-
-print('\nBenchmarking...')
-p_count = 20
-count = 200
+p_count = 22
+count = 500
+print('')
+print('Benchmarking with {} requests per connection, client_pki={!r}'.format(
+        count, options.client_pki)
+)
 for p in range(1, p_count + 1):
-    time.sleep(1)
     workers = []
+    time.sleep(1)  # Let things "settle" a moment
     start = time.time()
     for i in range(p):
-        w = start_process(loop, env, count)
+        w = start_process(client_process, env, count)
         workers.append(w)
     for w in workers:
         w.join()
     elapsed = time.time() - start
-    print('    Concurrency: {}; Requests Per Second: {:d}'.format(
+    print('    Concurrency: {}; Requests/Second: {:d}'.format(
             p, int((count * p) / elapsed)
         )
     )
