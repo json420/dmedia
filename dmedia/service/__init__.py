f2e3dd9c19a8ae5d970fcefbe2756ea343fe71e2
Jason Gerard DeRose <jderose@novacut.com>
2012-10-13 18:10:23 -0600

    Roughed out first run init into dmedia-service

--- dmedia/service/__init__.py
+++ dmedia/service/__init__.py
@@ -29,11 +29,26 @@ import dbus
 from dbus.mainloop.glib import DBusGMainLoop
 from gi.repository import GObject
 
-GObject.threads_init()
+
 DBusGMainLoop(set_as_default=True)
+GObject.threads_init()
 
 
 def get_proxy():
     session = dbus.SessionBus()
     return session.get_object('org.freedesktop.Dmedia', '/')
+
+    
+class FirstRun:
+    def __init__(self):
+        Dmedia = get_proxy()
+        if Dmedia.NeedsInit():
+            self.mainloop = GObject.MainLoop()
+            Dmedia.connect_to_signal('InitDone', self.on_InitDone)
+            Dmedia.DoInit()
+            self.mainloop.run()
+
+    def on_InitDone(self, env_s):
+        print(env_s)
+        self.mainloop.quit() 
 
