9c21ccf04c6eaac15134d93cb7976e5e90b1793f
Jason Gerard DeRose <jderose@novacut.com>
2013-03-25 20:56:31 -0600

    work-around for bzr not liking snapshoting with whami using the machine_id

--- dmedia/core.py
+++ dmedia/core.py
@@ -110,7 +110,7 @@ def snapshot_worker(env, dumpdir, queue_in, queue_out):
     server = Server(env)
     try:
         check_call(['bzr', 'init', dumpdir])
-        whoami = env['machine_id']
+        whoami = 'user@localhost'
         check_call(['bzr', 'whoami', '--branch', '-d', dumpdir, whoami])
         log.info('Initialized bzr branch in %r', dumpdir)
     except CalledProcessError:
