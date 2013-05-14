6b9fcbd6c33de6310a99050ac578094ac864ae18
Jason Gerard DeRose <jderose@novacut.com>
2013-05-14 15:05:19 -0600

    Removed most horrid get_filestore_id(), get_filestore(), and init_filestore() from util; removed their horrid tests as well

--- dmedia/util.py
+++ dmedia/util.py
@@ -43,45 +43,6 @@ def isfilestore(parentdir):
     return path.isdir(path.join(parentdir, DOTNAME))
 
 
-def get_filestore_id(parentdir):
-    files = path.join(parentdir, DOTNAME, 'files')
-    if is_v0_files(files):
-        fs = FileStore(parentdir)
-    store = path.join(parentdir, DOTNAME, 'store.json')
-    try:
-        doc = json.load(open(store, 'r'))
-        return doc['_id']
-    except Exception:
-        pass
-
-
-def get_filestore(parentdir, store_id, copies=None):
-    store = path.join(parentdir, DOTNAME, 'store.json')
-    doc = json.load(open(store, 'r'))
-    if doc['_id'] != store_id:
-        raise Exception(
-            'expected store_id {!r}; got {!r}'.format(store_id, doc['_id'])
-        )
-    if copies is not None:
-        doc['copies'] = copies
-    fs = FileStore(parentdir, doc['_id'], doc['copies'])
-    return (fs, doc)
-
-
-def init_filestore(parentdir, copies=1):
-    fs = FileStore(parentdir)
-    store = path.join(fs.basedir, 'store.json')
-    try:
-        doc = json.load(open(store, 'r'))
-    except Exception:
-        doc = schema.create_filestore(copies)
-        json.dump(doc, open(store, 'w'), sort_keys=True, indent=4)
-        os.chmod(store, stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH)
-    fs.id = doc['_id']
-    fs.copies = doc['copies']
-    return (fs, doc)
-
-
 def get_designs(db):
     rows = db.get('_all_docs', startkey='_design/', endkey='_design0')['rows']
     return dict(
