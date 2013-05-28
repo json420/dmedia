d9f2e0b2ba9af55d3608a05177f2b269c2de66dd
Jason Gerard DeRose <jderose@novacut.com>
2013-05-28 03:59:22 -0600

    Bumped version to 13.06

--- dmedia/__init__.py
+++ dmedia/__init__.py
@@ -28,7 +28,7 @@ WILL BREAK YOUR DMEDIA DATABASE AND COULD CAUSE DATA LOSS!
 
 from os import path
 
-__version__ = '13.05.0'
+__version__ = '13.06.0'
 BUS = 'org.freedesktop.Dmedia'
 TREE = path.dirname(path.dirname(path.abspath(__file__)))
 
