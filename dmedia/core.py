ff517b69b23cad6fbe9efbd4e4ffaf04af7534de
Jason Gerard DeRose <jderose@novacut.com>
2013-09-17 23:54:30 -0600

    core.downgrade_worker() now calls MetaStore.purge_by_store_atime(), and uses the same timestamp for all calls

--- dmedia/core.py
+++ dmedia/core.py
@@ -295,11 +295,14 @@ def vigilance_worker(env, ssl_config):
 
 def downgrade_worker(env):
     try:
+        curtime = int(time.time())
+        log.info('downgrading/purging as of timestamp %d', curtime)
         db = util.get_db(env)
         ms = MetaStore(db)
-        ms.downgrade_by_store_atime()
-        ms.downgrade_by_never_verified()
-        ms.downgrade_by_last_verified()
+        ms.downgrade_by_never_verified(curtime)
+        ms.downgrade_by_last_verified(curtime)
+        ms.downgrade_by_store_atime(curtime)
+        ms.purge_by_store_atime(curtime)
     except Exception:
         log.exception('Error in downgrade_worker():')
 
