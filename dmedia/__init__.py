e21d9ec0b6f9fda6ae43181c937aa339edcb2ccf
Jason Gerard DeRose <jason@system76.com>
2016-10-22 17:57:43 -0600

    Bump version to 16.11

--- dmedia/__init__.py
+++ dmedia/__init__.py
@@ -28,7 +28,7 @@ WILL BREAK YOUR DMEDIA DATABASE AND COULD CAUSE DATA LOSS!
 
 from os import path
 
-__version__ = '16.07.0'
+__version__ = '16.11.0'
 BUS = 'org.freedesktop.Dmedia'
 TREE = path.dirname(path.dirname(path.abspath(__file__)))
 
