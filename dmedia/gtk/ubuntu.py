7ed3684c4b5b1e62749210fbb770cad33869d25e
David Jordan <djordan2@novacut.com>
2019-11-23 22:07:19 -0700

    changes for bionic: remove Unity integration, update peering gui to webkit2, handle changes in the output of newer openssl version

--- dmedia/gtk/ubuntu.py
+++ dmedia/gtk/ubuntu.py
@@ -30,8 +30,8 @@ import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('Notify', '0.7')
 gi.require_version('AppIndicator3', '0.1')
-gi.require_version('Unity', '7.0')
-from gi.repository import Gtk, Notify, AppIndicator3, Unity
+#gi.require_version('Unity', '7.0')
+from gi.repository import Gtk, Notify, AppIndicator3 #, Unity
 
 from dmedia.units import bytes10
 from dmedia.misc import WeakMethod
@@ -123,7 +123,7 @@ def notify_stats(lines):
 class UnityImportUX:
     def __init__(self, hub):
         self.hub = hub
-        self.launcher = Unity.LauncherEntry.get_for_desktop_id('dmedia.desktop')
+        #self.launcher = Unity.LauncherEntry.get_for_desktop_id('dmedia.desktop')
         self.notify = NotifyManager()
         self.indicator = AppIndicator3.Indicator.new('dmedia', ICON,
             AppIndicator3.IndicatorCategory.APPLICATION_STATUS
@@ -144,18 +144,19 @@ class UnityImportUX:
         hub.connect('error', WeakMethod(self, 'on_error'))
 
     def __del__(self):
-        self.launcher.set_property('count_visible', False)
-        self.launcher.set_property('progress_visible', False)
+        #self.launcher.set_property('count_visible', False)
+        #self.launcher.set_property('progress_visible', False)
+        pass
 
     def on_stop(self, menuitem):
         self.hub.send('stop_importer')
 
     def on_batch_started(self, gm, batch_id):
         self.indicator.set_status(AppIndicator3.IndicatorStatus.ATTENTION)
-        self.launcher.set_property('count', 0)
-        self.launcher.set_property('count_visible', True)
-        self.launcher.set_property('progress', 0.0)
-        self.launcher.set_property('progress_visible', True)
+        #self.launcher.set_property('count', 0)
+        #self.launcher.set_property('count_visible', True)
+        #self.launcher.set_property('progress', 0.0)
+        #self.launcher.set_property('progress_visible', True)
         self.basedirs = []
 
     def on_import_started(self, gm, basedir, import_id, info):
@@ -168,20 +169,20 @@ class UnityImportUX:
         self.notify.replace(summary, body, icon)
 
     def on_batch_progress(self, gm, count, total_count, size, total_size):
-        self.launcher.set_property('count', total_count)
+        #self.launcher.set_property('count', total_count)
         progress = (0.0 if total_size == 0 else size / total_size)
-        self.launcher.set_property('progress', progress)
+        #self.launcher.set_property('progress', progress)
 
     def on_batch_finalized(self, gm, batch_id, stats, copies, lines):
-        self.launcher.set_property('count_visible', False)
-        self.launcher.set_property('progress_visible', False)
+        #self.launcher.set_property('count_visible', False)
+        #self.launcher.set_property('progress_visible', False)
         self.indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
         (summary, body) = notify_stats(lines)
         self.notify.replace(summary, body, 'notification-device-eject')
 
     def on_error(self, gm, error):
         self.indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
-        self.launcher.set_property('count_visible', False)
-        self.launcher.set_property('progress_visible', False)
-        self.launcher.set_property('urgent', True)
+        #self.launcher.set_property('count_visible', False)
+        #self.launcher.set_property('progress_visible', False)
+        #self.launcher.set_property('urgent', True)
         
